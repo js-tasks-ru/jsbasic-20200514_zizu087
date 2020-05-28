7af98b91601d8f49c9b73a8acba587de81e367ad
xena <zizu087@gmail.com>
2020-05-27 22:50:12 -0600

    Corrected the last symbol in the new string

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -6,7 +6,7 @@
  */
 function truncate(str, maxlength) {
   if (str.length > maxlength) {
-    return str.substr(0, maxlength-1) + `...`;
+    return str.substr(0, maxlength-1) + `…`;
 
   }
   return str;
