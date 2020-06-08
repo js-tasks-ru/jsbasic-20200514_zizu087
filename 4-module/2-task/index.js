dccb2781258282b1c1ac80d263a682268ab29223
xena <zizu087@gmail.com>
2020-06-08 09:55:33 -0600

    fixes 4 module

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -2,6 +2,7 @@
  * @param {HTMLTableElement} table
  * @return {void}
  */
+
 function makeDiagonalRed(table) {
   for (let i = 0; table.rows.length; i++ ) {
     table.rows[i].cells[i].style.background = `red`;
