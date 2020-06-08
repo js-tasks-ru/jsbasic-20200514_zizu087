adfa4e3ade2735e267796eb4fa48488bad95bbcc
xena <zizu087@gmail.com>
2020-06-08 10:14:34 -0600

    corrected a mistake in the for loop

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -2,3 +2,8 @@
  * @param {HTMLTableElement} table
  * @return {void}
  */
+ function makeDiagonalRed(table) {
+   for (let i = 0; i < table.rows.length; i++ ) {
+     table.rows[i].cells[i].style.background = `red`;
+   }
+ }
