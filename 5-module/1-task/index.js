fb0e53b2baf0261cf298abd69d0468ac85b12b39
xena <zizu087@gmail.com>
2020-06-10 14:29:32 -0600

    corrected the code so the classes are adde to the tr element

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -2,39 +2,29 @@
  * Метод устанавливает необходимые по условию аттрибуты таблице
  * @param {Element} table
  */
-function highlight(table) {
-  for (let i = 1; i < table.rows.length; i++) {
+ function highlight(table) {
+   for (let i = 1; i < table.rows.length; i++) {
 
-    if (!table.rows[i].cells[3].dataset.available) {
-     table.rows[i].cells[3].setAttribute(`hidden`, ``);
-    }
+     if (!table.rows[i].cells[3].dataset.available) {
+      table.rows[i].setAttribute(`hidden`, ``);
+     }
 
-    if (table.rows[i].cells[3].dataset.available === `true`) {
-     table.rows[i].cells[3].classList.add(`available`);
-    } else {
-     table.rows[i].cells[3].classList.add(`unavailable`);
-    };
+     if (table.rows[i].cells[3].dataset.available === `true`) {
+      table.rows[i].classList.add(`available`);
+     } else {
+      table.rows[i].classList.add(`unavailable`);
+     };
 
-    if (table.rows[i].cells[2].innerHTML === `m`) {
-     table.rows[i].cells[2].classList.add(`male`);
-    }
+     if (table.rows[i].cells[2].innerHTML === `m`) {
+      table.rows[i].classList.add(`male`);
+     }
 
-    if (table.rows[i].cells[2].innerHTML === `f`) {
-     table.rows[i].cells[2].classList.add(`female`);
-    }
+     if (table.rows[i].cells[2].innerHTML === `f`) {
+      table.rows[i].classList.add(`female`);
+     }
 
-    if (table.rows[i].cells[1].innerHTML < 18) {
-     table.rows[i].cells[1].style.textDecoration = `line-through`;
-    }
-  }
-}
-
-
-
-
-
-
-
-
-
-*/
+     if (table.rows[i].cells[1].innerHTML < 18) {
+      table.rows[i].style.textDecoration = `line-through`;
+     }
+   }
+ }
